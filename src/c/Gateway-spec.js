3c2182733231b40efb2bd5cf37ce5cab95fd0e45
Callan Peter Milne <callan.milne@eviratec.com.au>
2017-06-02 14:33:50 +1000

    Starting to add tests

---
+++ src/c/Gateway-spec.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const Gateway
+const GatewayInterface
+const GatewayRegistry
+
+describe("Gateway", function () {
+
+  beforeEach(function () {
+
+  });
+
+  describe("gateway.registryFor(type)", function () {
+    
+  });
+
+});
--- src/c/GatewayInterface.js
+++ src/c/GatewayInterface.js
@@ -5,7 +5,11 @@ const EventEmitter = require('events');
 module.exports = class GatewayInterface extends EventEmitter () {
 
   constructor () {
+
     super();
+
+    this.target = null;
+
   }
 
   static is (input) {
